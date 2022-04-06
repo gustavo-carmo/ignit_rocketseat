c6cc2d8c01bb45e6704c83843b6bba21d9c8b9bb
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2022-04-06 21:07:44 +0100

    add cors to application

--- api-rentx/src/shared/infra/http/app.ts
+++ api-rentx/src/shared/infra/http/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
@@ -18,19 +19,20 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/car', express.static(`${upload.tmpFolder}/car`));
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(routes);
 
+const startDate = new Date();
 app.use('/', (request, response) => {
   return response
     .status(200)
-    .send(`<h3>Application running since ${new Date()}</h3>`);
+    .send(`<h3>Application running since ${startDate}</h3>`);
 });
-
-app.use(routes);
 
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {

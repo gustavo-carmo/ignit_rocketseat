547da11227abfe40ef7d9ecc6cd0c5585742f3ad
Gustavo Carmo <gustavo.silva.carmo@hotmail.com>
2022-05-12 00:02:52 +0100

    change the router webhooks to buffer the events

--- Next/ig-news/src/pages/api/webhooks.ts
+++ Next/ig-news/src/pages/api/webhooks.ts
@@ -1,9 +1,64 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const relevantEvents = new Set(["checkout.session.completed"]);
 
 const webhooks = async (request: NextApiRequest, response: NextApiResponse) => {
-  console.log("Webhooks acessiveis");
+  if (request.method === "POST") {
+    const buf = await buffer(request);
+    const secret = request.headers["stripe-signature"];
 
-  return response.status(204).end();
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      return response.status(400).send(`Webhook error: ${err.message}`);
+    }
+
+    const { type } = event;
+
+    if (relevantEvents.has(type)) {
+      try {
+        switch (type) {
+          case "checkout.session.completed":
+            break;
+          default:
+            throw new Error("Unhandled event.");
+        }
+      } catch (err) {
+        return response.json({ error: "Webhook handler failed" });
+      }
+    }
+
+    return response.status(200).json({ received: true });
+  } else {
+    response.setHeader("Allow", "POST");
+
+    return response.status(2405).end("Method not allowed");
+  }
 };
 
 export default webhooks;
